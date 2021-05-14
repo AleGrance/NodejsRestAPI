77c2dc68210096f05d5d9d06ddbe9f725be53590
Ale Grance <alegrance1000@gmail.com>
2021-05-14 13:52:20 -0400

    Cross Domain

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import consign from 'consign'; //para estructurar mejor el proyecto
 
 const app = express();
 
+//cross access
+var allowCrossDomain = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+}
+app.use(allowCrossDomain);
+
 consign({
-    cwd: __dirname
-})
+        cwd: __dirname
+    })
     .include('libs/config.js')
     .then('db.js')
     .then('libs/middlewares.js') //se configura el puerto
--- src/routes/users.js
+++ src/routes/users.js
@@ -31,24 +31,24 @@ module.exports = app => {
         })
         .put((req, res) => {
             Users.update(req.body, {
-                where: req.params
-            })
-            .then(result => res.sendStatus(204))
-            .catch(error => {
-                res.status(412).json({
-                    msg: error.message
-                });
-            })
+                    where: req.params
+                })
+                .then(result => res.sendStatus(204))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                })
         })
         .delete((req, res) => {
             Users.destroy({
-                where: req.params
-            })
-            .then(result => res.json(result))
-            .catch(error => {
-                res.status(412).json({
-                    msg: error.message
-                });
-            })
+                    where: req.params
+                })
+                .then(result => res.json(result))
+                .catch(error => {
+                    res.status(412).json({
+                        msg: error.message
+                    });
+                })
         })
 };
